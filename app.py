40db53eb3c82eb6e6ad66930a86515eb014b35c9
Zain3226 <zain_rafique.ahmed@hotmail.com>
2022-06-03 01:17:44 +0500

    v1

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-
 import pandas as pd
 import altair as alt
 import streamlit as st
@@ -14,9 +13,11 @@ year_SR_df=df.groupby(df['year'])['suicides_no'].sum().reset_index().sort_values
 Total_suicide=year_SR_df['suicides_no'].sum()
 Total_suicide=numerize.numerize(Decimal(float(Total_suicide)))
 
-
+# Show total Number of Suicides
 st.metric(label="Number Suicides from 1987-2016", value=Total_suicide)
 
+
+# Suicide year wise
 st.subheader('Suicide rate per year')
 chart = alt.Chart(year_SR_df).mark_bar().encode(
   x=alt.X('year'),
@@ -24,6 +25,8 @@ chart = alt.Chart(year_SR_df).mark_bar().encode(
 )
 st.altair_chart(chart, use_container_width=True)
 
+
+# Suicide Rate in gender(col1) suicide rate in age group(col2)
 col1, col2= st.columns(2)
 with col1:
     gender_ratio = df.groupby('sex')['suicides_no'].sum().reset_index()
@@ -42,6 +45,8 @@ with col2:
     )
     st.altair_chart(chart, use_container_width=True)
 
+
+# Top 10 Countries with High Suicide Rate'
 st.subheader('Top 10 Countries with High Suicide Rate')
 country_df=df.groupby('country')['suicides_no'].sum().reset_index()
 country_df=country_df.sort_values('suicides_no',ascending=False)
@@ -51,6 +56,7 @@ chart = alt.Chart(country_df.head(10)).mark_bar().encode(
 )
 st.altair_chart(chart, use_container_width=True)
 
+# Top 10 Countries with High Suicide Rate in Males and Females'
 gender_ratio=df.groupby(['country','sex'])['suicides_no'].sum().reset_index()
 gender_ratio=gender_ratio.sort_values('suicides_no',ascending=False)
 
@@ -71,9 +77,38 @@ with col2:
     st.altair_chart(chart, use_container_width=True)
 
 
+# year wise ratio in countries and gender
+Select_year=df.groupby(['year','sex','country'])['suicides_no'].sum().reset_index().sort_values('suicides_no',ascending=False)
+
+year_option = st.selectbox(
+     'Select Year',
+     df['year'].drop_duplicates())
+
+st.subheader('Top 10 Countries Suicide rate in '+str(year_option))
+
+chart = alt.Chart(Select_year[Select_year['year']==year_option].head(10)).mark_bar().encode(
+        x=alt.X('country',sort=None),
+        y=alt.Y('suicides_no')
+    ).interactive()
+st.altair_chart(chart, use_container_width=True)
 
 
 
+col1,col2=st.columns(2)
+with col1:
+    st.write('Top 10 countries in {} with a High suicide rate among Males'.format(year_option))
+    chart = alt.Chart(Select_year[(Select_year['sex']=='male')&(Select_year['year']==year_option)].head(10)).mark_bar().encode(
+        x=alt.X('country',sort=None),
+        y=alt.Y('suicides_no')
+    )
+    st.altair_chart(chart, use_container_width=True)
+with col2:
+    st.write('Top 10 countries in {} with a High suicide rate among Females'.format(year_option))
+    chart = alt.Chart(Select_year[(Select_year['sex']=='female')&(Select_year['year']==year_option)].head(10)).mark_bar().encode(
+        x=alt.X('country',sort=None),
+        y=alt.Y('suicides_no')
+    )
+    st.altair_chart(chart, use_container_width=True)
 
 
 
